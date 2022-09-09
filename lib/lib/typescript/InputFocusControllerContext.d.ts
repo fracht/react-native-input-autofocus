32c79a13c97394fbfddcde2ff28363f8580e4e09
Aleksandras Bogdanovas <aleksandras.bogdanovas21@gmail.com>
2022-09-09 17:38:22 +0300

    Created custom hool

--- lib/lib/typescript/InputFocusControllerContext.d.ts
+++ lib/lib/typescript/InputFocusControllerContext.d.ts
@@ -1,10 +1,10 @@
 import { RefObject } from 'react';
-import { NativeMethods } from 'react-native';
+import { TextInput } from 'react-native';
 import { Node } from './InputFocusController';
 declare type InputFocusControllerContextType = {
-    register: (ref: RefObject<NativeMethods | undefined>) => Node<React.RefObject<NativeMethods | undefined>>;
-    unregister: (node: Node<RefObject<NativeMethods | undefined>>) => void;
-    createOnSubmitEditing: (node: Node<RefObject<NativeMethods | undefined>>) => () => void;
+    register: (ref: RefObject<TextInput | undefined>) => Node<React.RefObject<TextInput | undefined>>;
+    unregister: (node: Node<RefObject<TextInput | undefined>>) => void;
+    createOnSubmitEditing: (node: Node<RefObject<TextInput | undefined>>) => () => void;
 };
 export declare const InputFocusControllerContext: import("@sirse-dev/safe-context").SafeContext<InputFocusControllerContextType>;
 export declare const useInputFocusControllerContext: () => InputFocusControllerContextType;
